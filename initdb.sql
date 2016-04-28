d4211710da8cbe2bb6f466049f9e41e1fb7a79f7
Anton Paulsson <Anton.Zichi@gmail.com>
2016-04-28 20:30:30 +0200

    syntax fix

--- initdb.sql
+++ initdb.sql
@@ -1,18 +1,21 @@
 /* Initial database table structure */
-
 CREATE TABLE IF NOT EXISTS servers(
+    id INTEGER AUTO_INCREMENT,
 	ip VARCHAR(50) NOT NULL,
 	port VARCHAR(5) NOT NULL,
 	name VARCHAR(50) NOT NULL,
 	section VARCHAR(50) NOT NULL,
-	id INTEGER AUTO_INCREMENT 
+    
+    PRIMARY KEY(id)
 )ENGINE=INNODB;
 
 CREATE TABLE IF NOT EXISTS options(
+    id INTEGER AUTO_INCREMENT,
 	label VARCHAR(50) NOT NULL,
-	option VARCHAR(20) NOT NULL,
-	id INTEGER AUTO_INCREMENT
+	value VARCHAR(20) NOT NULL,
+    
+    PRIMARY KEY(id)
 )ENGINE=INNODB;
 
 /* Inserts the options base value */
-INSERT INTO options ('label', 'option') VALUES ('minimal', false),('refreshtimer', '30'),('showMS', true);
+INSERT INTO options(label, value)VALUES('minimal', false),('refreshtimer', '30'),('showMS', true);
